300dbf9cea2a3844b6b0f16fc2f4a8a1483e4dd7
DougBica <doug.bica@hotmail.com>
2020-06-19 20:49:52 -0300

    primeiro comum

---
+++ src/main/java/br/com/caronapp/CaronappApplication.java
@@ -0,0 +1,13 @@
+package br.com.caronapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaronappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaronappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/caronapp/CaronappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caronapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaronappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
